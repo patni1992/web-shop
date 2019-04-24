8dd56f08abb8eac584faae60e37f504273536f9d
Patrik <patriknilsson1992@gmail.com>
2019-04-24 11:24:21 +0700

    replace header with components from Bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap"
+
 function Header() {
     return (
-        <div>
-            <ul>
-                <li>
-                    <Link to="/">Product catalog</Link>
-                </li>
-                <li>
-                    <Link to="/basket-summary">Basket summary</Link>
-                </li>
-            </ul>
-            <hr />
-        </div>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand as={NavLink} to="/" >Simple Web Shop</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link as={NavLink} to="/">Product catalog</Nav.Link>
+                    <Nav.Link as={NavLink} to="/basket-summary">Basket summary</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
-
 
 export default Header;
