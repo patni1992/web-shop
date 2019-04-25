38beaec314f4a49f71ba5158462f0c1a0ce8a943
Patrik <patriknilsson1992@gmail.com>
2019-04-25 21:42:06 +0700

    add methods to calculate dates

---
+++ src/helpers/index.js
@@ -0,0 +1,32 @@
+export function daysBetween(date1, date2) {
+    var oneDay = 1000 * 60 * 60 * 24;
+    var date1Ms = date1.getTime();
+    var date2Ms = date2.getTime();
+    var diffMs = Math.abs(date1Ms - date2Ms);
+
+    return Math.round(diffMs / oneDay);
+}
+
+export function calcuateDeliverDate(shipDays) {
+    shipDays = parseInt(shipDays)
+    const today = new Date();
+    const nextTuseday = new Date();
+    nextTuseday.setDate(nextTuseday.getDate() + (2 + 7 - nextTuseday.getDay()) % 7);
+    
+    const daysUntilsShip = daysBetween(today, nextTuseday);
+    let finalDate = new Date(today.setDate(today.getDate() + (daysUntilsShip + shipDays)))
+    
+    const isSaturday = new Date(finalDate).getDay() === 6
+    const isSunday = new Date(finalDate).getDay() === 0
+
+    if (isSaturday) {
+        finalDate = new Date(finalDate.setDate(finalDate.getDate() + 2));
+    } else if (isSunday) {
+        finalDate = new Date(finalDate.setDate(finalDate.getDate() + 1));
+    }
+
+    return {
+        date: finalDate,
+        countDays: daysBetween(today, finalDate)
+    }
+}
