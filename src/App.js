0e0835f54b7ea3880c50a4dad03daa8a3cd8b202
Patrik <patriknilsson1992@gmail.com>
2019-04-25 15:27:17 +0700

    fetch products and categories with axios

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from "react";
+import axios from "axios";
 import ProductCatalog from "./components/ProductCatalog";
 import BasketSummary from "./components/BasketSummary";
 import Header from "./components/Header";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <Router>
-      <Header />
-      <Route exact path="/" component={ProductCatalog} />
-      <Route path="/basket-summary" component={BasketSummary} />
-    </Router>
-  );
-}
+class App extends React.Component {
+  state = {
+    products: []
+  };
+  componentDidMount() {
+    axios.get(`products.json`).then(res => {
+      console.log("products");
+      this.setState({
+        products: res.data
+      });
+      console.log(res);
+    });
 
+    axios.get(`categories.json`).then(res => {
+      console.log("categories");
+      console.log(res);
+      
+      console.log(this.list_to_tree(res.data));
+    });
+  }
+  render() {
+    return (
+      <Router>
+        <Header />
+        <Route exact path="/" component={ProductCatalog} />
+        <Route path="/basket-summary" component={BasketSummary} />
+      </Router>
+    );
+  }
+}
 export default App;
